8695d08d021e8c4d6e6efcc07b626ec70efad016
atieh-mir <atiehmir17@gmail.com>
2023-10-28 23:50:33 +0330

    character counter

--- character counter/src/App.jsx
+++ character counter/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-
+import { Form, Footer, Header } from "./components";
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleOnChange = ({target}) => {
+    setCount(target.value.length)
+  }
   return (
     <>
-     <div>hii</div>
+    <h1>Character counter</h1>
+    <form onChange={handleOnChange}>
+      <textarea placeholder='write some text..'></textarea>
+    </form>
+    <h3>Your character counter : {count} </h3>
     </>
   )
 }
